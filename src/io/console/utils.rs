be1aecc8a413c555c058d2a534e76d912f55a02d
pepedinho <2spii94@gmail.com>
2025-10-21 21:22:47 +0200

    io/console: directionnal arrow gesture

--- src/io/console/utils.rs
+++ src/io/console/utils.rs
@@ -6,7 +6,7 @@ pub trait U8CellLen {
 
 impl<const N: usize> U8CellLen for [Cell; N] {
     fn cell_len(&self) -> usize {
-        let len = self.iter().position(|e| e.byte == 0).unwrap_or(N);
+        let len = self.iter().position(|e| e.byte == 0).unwrap_or(0);
         if len == VGA_WIDTH { VGA_WIDTH - 1 } else { len }
     }
 }
