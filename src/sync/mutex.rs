e602d195403a25eb0a3f63e12c00acfe99433c98
pepedinho <2spii94@gmail.com>
2025-10-24 14:01:38 +0200

    sync/mutex: complete mutex implementation

--- src/sync/mutex.rs
+++ src/sync/mutex.rs
@@ -1,23 +1,35 @@
 use core::{
     cell::UnsafeCell,
     ops::{Deref, DerefMut},
+    sync::atomic::{AtomicBool, Ordering},
 };
 
 pub struct Mutex<T> {
+    lock: AtomicBool,
     data: UnsafeCell<T>,
 }
 
-unsafe impl<T: Send> Sync for Mutex<T> {}
-unsafe impl<T: Send> Send for Mutex<T> {}
+unsafe impl<T> Sync for Mutex<T> where T: Send {}
 
 impl<T> Mutex<T> {
     pub const fn new(data: T) -> Self {
         Self {
+            lock: AtomicBool::new(false),
             data: UnsafeCell::new(data),
         }
     }
 
     pub fn lock(&self) -> MutexGuard<'_, T> {
+        loop {
+            if self
+                .lock
+                .compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed)
+                .is_ok()
+            {
+                break;
+            }
+        }
+
         MutexGuard { mutex: self }
     }
 }
@@ -26,15 +38,22 @@ pub struct MutexGuard<'a, T> {
     mutex: &'a Mutex<T>,
 }
 
-impl<'a, T> Deref for MutexGuard<'a, T> {
+impl<T> Deref for MutexGuard<'_, T> {
     type Target = T;
+
     fn deref(&self) -> &Self::Target {
         unsafe { &*self.mutex.data.get() }
     }
 }
 
-impl<'a, T> DerefMut for MutexGuard<'a, T> {
+impl<T> DerefMut for MutexGuard<'_, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         unsafe { &mut *self.mutex.data.get() }
+    }
+}
+
+impl<T> Drop for MutexGuard<'_, T> {
+    fn drop(&mut self) {
+        self.mutex.lock.store(false, Ordering::Release);
     }
 }
